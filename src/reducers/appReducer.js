eb94505c6e5e3ff6d405b4c801e5a8e1a9ca403d
VietAn98 <44429962+VietAn98@users.noreply.github.com>
2019-10-06 19:25:52 +0700

    Tạo các Redux reducer

---
+++ src/reducers/appReducer.js
@@ -0,0 +1,94 @@
+const initialState = {
+  history: [
+    {
+      squares: Array(400).fill(null)
+    }
+  ],
+  isNext: true,
+  win: false,
+  stepNumber: 0,
+  checkWin: false,
+  winSquares: [],
+  winSquaresTemp: [],
+  isUp: true,
+  isDown: false
+};
+
+const appReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case 'SAVE_HISTORY':
+      return {
+        ...state,
+        history: action.history.concat([
+          {
+            squares: action.squares
+          }
+        ]),
+        stepNumber: action.history.length,
+        isNext: !action.isNext
+      };
+
+    case 'CHECK_AS_WIN':
+      return {
+        ...state,
+        winSquares: action.winSquares,
+        checkWin: true,
+        winSquaresTemp: action.winSquares,
+        win: true
+      };
+
+    case 'CHECK_IF_LAST_STEP':
+      return {
+        ...state,
+        stepNumber: action.step,
+        isNext: action.step % 2 === 0,
+        winSquares: action.winSquaresTemp,
+        checkWin: true
+      };
+
+    case 'CHECK_IF_NOT_LAST_STEP':
+      return {
+        ...state,
+        stepNumber: action.step,
+        isNext: action.step % 2 === 0,
+        winSquares: [],
+        checkWin: false
+      };
+
+    case 'RESET_SQUARES':
+      return {
+        ...state,
+        history: [
+          {
+            squares: Array(400).fill(null)
+          }
+        ],
+        isNext: true,
+        win: false,
+        stepNumber: 0,
+        checkWin: false,
+        winSquares: [],
+        winSquaresTemp: [],
+        isUp: true,
+        isDown: false
+      };
+
+    case 'ASCENDING_SORT':
+      return {
+        ...state,
+        isUp: true,
+        isDown: false
+      };
+
+    case 'DECREASING_SORT':
+      return {
+        ...state,
+        isDown: true,
+        isUp: false
+      };
+    default:
+      return state;
+  }
+};
+
+export default appReducer;
