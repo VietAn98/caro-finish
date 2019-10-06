6e73ce47a62286b27daa824a5db6f6ebb849049b
VietAn98 <44429962+VietAn98@users.noreply.github.com>
2019-10-06 19:23:15 +0700

    Tổ chức thư mục bao gồm: actions, reducers, components, containers

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Swal from "sweetalert2";
 import "./App.css";
-import Board from "./components/Board";
+import Board from "../components/Board";
 
 class App extends React.Component {
   constructor(props) {
